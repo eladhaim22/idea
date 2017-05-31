a467ce15d1fc338ab0fbbfdbedb7a7b06d62c372
Elad <eladhaim22@gmail.com>
2017-05-31 00:26:13 -0300

    cambios scripts base de datos

---
+++ src/main/resources/config/liquibase/changelog/Users0.script.sql
@@ -0,0 +1,2 @@
+Insert into Users (login,password_hash,first_name,last_name,email,image_url,activated,lang_key,created_by,last_modified_by) Values 
+('system','$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG','System','System','system@localhost','',1,'en','system','system')
